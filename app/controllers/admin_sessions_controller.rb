16b3853888d4788c262fa8b38222e4a2a38f8dc4
DYY <dengyanyi21@163.com>
2019-06-20 10:20:50 +0800

    AdminLogin

--- app/controllers/admin_sessions_controller.rb
+++ app/controllers/admin_sessions_controller.rb
@@ -4,9 +4,20 @@ class AdminSessionsController < ApplicationController
   end
 
   def create
+    name = params[:admin_session][:name]
+    password = params[:admin_session][:password]
+    admin = Admin.find_by_name(name)
+    if admin && admin.authenticate(password)
+      log_in(admin)
+      redirect_to colleges_path
+    else
+      flash[:notice] = "Invalid login or password."
+      render 'new'
+    end
   end
 
   def destroy
-
+    log_out if logged_in?
+    redirect_to root_path
   end
 end
--- app/helpers/admin_sessions_helper.rb
+++ app/helpers/admin_sessions_helper.rb
@@ -1,2 +1,19 @@
 module AdminSessionsHelper
+  def log_in(admin)
+    session[:admin_id] = admin.id
+  end
+
+  def current_user
+    if session[:admin_id] != nil
+      @current_admin = Admin.find_by_id(session[:admin_id])
+    else
+      @current_admin = nil
+    end
+    @current_admin
+  end
+
+  #用来判断用户是否登录的方法
+  def logged_in?
+    !current_user.nil?
+  end
 end
