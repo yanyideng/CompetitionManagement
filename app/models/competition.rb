fbfa47fb714b3b698d1431bb9dd69c3b5dc1ec03
George520 <zishichangsheng@gmail.com>
2019-06-20 16:44:54 +0800

    后台添加教师表和竞赛表

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,3 +1,7 @@
 class Competition < ApplicationRecord
   has_many :groups, dependent: :destroy
+  validates :name, presence: true
+  validates :version, presence: true
+  validates :deadline, presence: true
+  validates :endtime, presence: true
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,7 @@
 class Teacher < ApplicationRecord
   has_many :groups, through: :guide_teachers
   belongs_to :college
+
+  validates :teacher_id, presence: true
+  validates :name, presence: true
 end
