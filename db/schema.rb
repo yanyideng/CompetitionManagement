eeb903a18b9a241fa7d8fad18bf9b4ecc0d0d8a9
DYY <dengyanyi21@163.com>
2019-06-20 08:21:04 +0800

    Change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_155825) do
+ActiveRecord::Schema.define(version: 2019_06_19_162247) do
 
   create_table "achievements", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2019_06_19_155825) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["college_id"], name: "index_students_on_college_id"
+    t.index ["student_id"], name: "index_students_on_student_id"
   end
 
   create_table "teachers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -77,6 +78,7 @@ ActiveRecord::Schema.define(version: 2019_06_19_155825) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["college_id"], name: "index_teachers_on_college_id"
+    t.index ["teacher_id"], name: "index_teachers_on_teacher_id"
   end
 
   add_foreign_key "achievements", "groups"
