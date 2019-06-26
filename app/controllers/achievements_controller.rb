22e962df1fc01a9624efaaf4d453bb1ca9f93453
DYY <dengyanyi21@163.com>
2019-06-26 10:55:58 +0800

    Feature:SearchAcievements

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,6 +1,15 @@
 class AchievementsController < ApplicationController
   def index
-    @achievements = Achievement.all
+    competitions = Competition.all
+    @select_array = Array.new
+    @select_array << ['全部', '']
+    competitions.each do |competition|
+      competition_full_name = '第' + competition.version.to_s + '届' + competition.name
+      @select_array << [competition_full_name, competition.id]
+    end
+    conditions = params.slice(:competition_id, :prize)
+    conditions.each { |k, v| conditions.delete(k) if v.empty? }
+    @achievements = Achievement.filter(conditions)
   end
 
   def show
--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,3 +1,17 @@
 class Achievement < ApplicationRecord
   belongs_to :group
+
+  def self.filter(cons = {})
+    _self = self # Achievement
+    comps = Competition.all
+    groups = Group.all
+    # 根据条件查询
+    # 比赛
+    comps = comps.where(id: cons[:competition_id]) if cons[:competition_id]
+    # 小组
+    groups = groups.where(competition_id: comps.ids) unless comps.empty?
+    _self = _self.where(group_id: groups.ids)
+    _self = _self.where(prize: cons[:prize]) if cons[:prize]
+    _self
+  end
 end
